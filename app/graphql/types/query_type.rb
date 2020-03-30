08417acc292f3c559438da9a5b5cb7fde0cd9487
Marco-FI-SEI <mmc90@hotmail.co.uk>
2020-03-30 14:29:26 +0100

    📦 NEW: Add: RSpec and basic query test

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,7 +6,17 @@ module Types
           description: "Returns a list of todos"
 
     def todos
-      Todo.all
+      # Todo.all
+      # Eager load users - can be improved with batch loading!
+      Todo.preload(:user)
+    end
+
+    field :todo, Types::TodoType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def todo(id: )
+      Todo.find(id)
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :todos, dependent: :destroy
+
+  def full_name
+
+  end
 end
